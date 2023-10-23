1597e1a530aea9b28c4d9dc5c34f1a41f0a4f5dc
André Jesus <andrejesus2003@gmail.com>
2023-10-23 14:16:49 +0100

    tina

---
+++ src/pt/ulusofona/lp2/deisichess/TestGameManager.java
@@ -0,0 +1,10 @@
+package pt.ulusofona.lp2.deisichess;
+
+public class TestGameManager {
+    /*a funcao de teste nao pode tar vazia
+    a funcao de teste tem de ter pelo menos um assert
+    a funcao de teste tem de chamar codigo do projeto
+    o assert tem de comparar o resultado de uma funcao do projeto com um resultado esperado
+    */
+
+}
