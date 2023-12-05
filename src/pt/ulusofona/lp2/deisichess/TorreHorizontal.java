f9dfa0a7f6dcb4393e75a164c39d55b89084ac6a
Tomas Nave <tomasdasilvanave@gmail.com>
2023-12-05 21:11:43 +0000

    moveTorreHorizontal

--- src/pt/ulusofona/lp2/deisichess/TorreHorizontal.java
+++ src/pt/ulusofona/lp2/deisichess/TorreHorizontal.java
@@ -14,9 +14,34 @@ public class TorreHorizontal extends Peca {
 
     @Override
     boolean validMove(int x, int y, Tabuleiro tabuleiro) {
-        return false;
-    }
+        // Verificar se a posição de destino está dentro do tabuleiro
+        if (x < 0 || x >= tabuleiro.getTamanho() || y < 0 || y >= tabuleiro.getTamanho()) {
+            return false;
+        }
 
+        // Verificar se a posição de destino está na mesma linha horizontal
+        if (y != getY()) {
+            return false;
+        }
+
+        int deltaX = Math.abs(x - getX());
+
+        // Verificar se há peças no caminho
+        int stepX = (x - getX()) / Math.max(1, deltaX);
+
+        for (int i = 1; i < deltaX; i++) {
+            int currentX = getX() + i * stepX;
+
+            // Verificar se há uma peça na posição atual do caminho
+            if (tabuleiro.getPeca(currentX, y) != null) {
+                return false;
+            }
+        }
+
+        // Verificar se a posição de destino está vazia ou contém uma peça adversária
+        Peca pecaDestino = tabuleiro.getPeca(x, y);
+        return pecaDestino == null || pecaDestino.getEquipa() != getEquipa();
+    }
 
 
 
