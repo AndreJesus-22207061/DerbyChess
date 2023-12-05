972647861d8b63081f20e2322a5515d440bf4889
André Jesus <andrejesus2003@gmail.com>
2023-12-05 21:36:16 +0000

    Move TorreVertical

--- src/pt/ulusofona/lp2/deisichess/TorreVertical.java
+++ src/pt/ulusofona/lp2/deisichess/TorreVertical.java
@@ -13,6 +13,32 @@ public class TorreVertical extends Peca{
 
     @Override
     boolean validMove(int x, int y, Tabuleiro tabuleiro) {
-        return false;
+        // Verificar se a posição de destino está dentro do tabuleiro
+        if (x < 0 || x >= tabuleiro.getTamanho() || y < 0 || y >= tabuleiro.getTamanho()) {
+            return false;
+        }
+
+        // Verificar se a posição de destino está na mesma coluna vertical
+        if (x != getX()) {
+            return false;
+        }
+
+        int deltaY = Math.abs(y - getY());
+
+        // Verificar se há peças no caminho
+        int stepY = (y - getY()) / Math.max(1, deltaY);
+
+        for (int i = 1; i < deltaY; i++) {
+            int currentY = getY() + i * stepY;
+
+            // Verificar se há uma peça na posição atual do caminho
+            if (tabuleiro.getPeca(x, currentY) != null) {
+                return false;
+            }
+        }
+
+        // Verificar se a posição de destino está vazia ou contém uma peça adversária
+        Peca pecaDestino = tabuleiro.getPeca(x, y);
+        return pecaDestino == null || pecaDestino.getEquipa() != getEquipa();
     }
 }
