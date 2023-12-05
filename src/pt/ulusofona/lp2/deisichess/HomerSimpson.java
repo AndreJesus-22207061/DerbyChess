41b0aa9a7e9d96eddffe923730773ad2b9457fc1
Tomas Nave <tomasdasilvanave@gmail.com>
2023-12-05 22:15:37 +0000

    movePadre

--- src/pt/ulusofona/lp2/deisichess/HomerSimpson.java
+++ src/pt/ulusofona/lp2/deisichess/HomerSimpson.java
@@ -13,6 +13,44 @@ public class HomerSimpson extends Peca{
 
     @Override
     boolean validMove(int x, int y, Tabuleiro tabuleiro) {
+
+        ContadorJogadas contadorJogadas = tabuleiro.getContadorJogadas();
+
+        int ronda = contadorJogadas.getRondaAtual();
+
+        if ((x < 0 || x >= tabuleiro.getTamanho() || y < 0 || y >= tabuleiro.getTamanho())) {
+            return false;
+        }
+
+        if(ronda%3==0){
+            return false;
+        }
+
+        if ((x == this.x && (y == this.y + 1 || y == this.y - 1))) {
+            // Movimento vertical //
+
+            return true;
+        }
+
+        if (y == this.y && (x == this.x + 1 || x == this.x - 1)) {
+            // Movimento horizontal //
+            return true;
+        }
+
+        if ((x == this.x + 1 || x == this.x - 1) && (y == this.y + 1 || y == this.y - 1)) {
+            // Movimento diagonal //
+            return true;
+        }
+
+
+
+
+
+
+
+
+
+
         return false;
     }
 }
