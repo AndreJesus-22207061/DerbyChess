1f434dfb80525df3f2ea9bf7a71b8b0326a8364f
Tomas Nave <tomasdasilvanave@gmail.com>
2023-12-20 22:44:33 +0000

    funcional modificado

--- src/pt/ulusofona/lp2/deisichess/StatisticsKt.kt
+++ src/pt/ulusofona/lp2/deisichess/StatisticsKt.kt
@@ -2,7 +2,7 @@ package pt.ulusofona.lp2.deisichess
 
 class StatisticsKt {
 
-    fun  getStatsCalculator(tipo: StatType): Function1<GameManager, Array<String>> {
+    fun  getStatsCalculator(tipo: StatType): Function1<GameManager,ArrayList<String>> {
         when (tipo) {
             StatType.TOP_5_PONTOS -> return ::calculaMaximo
             StatType.TOP_5_CAPTURAS -> return ::calculaMaximo
@@ -14,10 +14,9 @@ class StatisticsKt {
     }
 
 
-    fun calculaMaximo(numeros: GameManager) : Array<String> {
-        var array = arrayOf<String>()
-
-        return array
+    fun calculaMaximo(numeros: GameManager) : ArrayList<String> {
+        var arrayList = ArrayList<String>()
+        return  arrayList
     }
 
 
