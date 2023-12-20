7385d010e24b2a3bfd6687e00b467ffb7fb1da4f
Tomas Nave <tomasdasilvanave@gmail.com>
2023-12-20 23:31:53 +0000

    alteracao invalidGame

--- src/pt/ulusofona/lp2/deisichess/InvalidGameInputException.java
+++ src/pt/ulusofona/lp2/deisichess/InvalidGameInputException.java
@@ -1,4 +1,18 @@
 package pt.ulusofona.lp2.deisichess;
 
 public class InvalidGameInputException extends Throwable {
+
+   String getProblemDescription(){
+
+       return "Dibil";
+   }
+
+   int  getLineWithError(){
+
+       return 0;
+   }
+
+
+
+
 }
