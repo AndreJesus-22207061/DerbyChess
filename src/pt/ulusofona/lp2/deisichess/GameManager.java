d8c2f0c37f85242bd3d6df355880730bfb27917f
Tomas Nave <tomasdasilvanave@gmail.com>
2023-12-21 12:47:01 +0000

    Undo alteracao

--- src/pt/ulusofona/lp2/deisichess/GameManager.java
+++ src/pt/ulusofona/lp2/deisichess/GameManager.java
@@ -321,19 +321,22 @@ public class GameManager {
         if (tabuleiro != null) {
             Jogada ultimaJogada = tabuleiro.obterUltimaJogada();
 
-            ContadorRondas contadorRondas = tabuleiro.getContadorRondas();
-            contadorRondas.decrementaRondaAtual();
-            contadorRondas.decrementaRondaJoker();
-
-            if(!ultimaJogada.houveCaptura()){
-                contadorRondas.decrementaRondasSemCaptura();
-            }
-
-
-
-
-
             if (ultimaJogada != null) { // Se existe uma jogada no historico
+                ContadorRondas contadorRondas = tabuleiro.getContadorRondas();
+                contadorRondas.decrementaRondaAtual();
+                contadorRondas.decrementaRondaJoker();
+
+                CountJogadas countPreta = tabuleiro.getContadorEquipa(10);  //caso se crie uma nova equipa criar adicionar countNovo
+                CountJogadas countBranca = tabuleiro.getContadorEquipa(20);
+
+                if((!ultimaJogada.houveCaptura() && countBranca.getPecasCapturadas() >= 1) || ((!ultimaJogada.houveCaptura() && countPreta.getPecasCapturadas() >=1))){
+                    contadorRondas.decrementaRondasSemCaptura();
+                }
+
+
+
+
+
                 tabuleiro.restaurarTabuleiro(ultimaJogada);
 
                 tabuleiro.mudarEquipaAjogar();
